091ffbfe347eeab295f64449136bdc5e543cbc51
bbouley <b.bouley@hotmail.co.uk>
2016-06-10 21:18:17 -0600

    initial setup with models

---
+++ src/server/models/draws.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = new mongoose.Schema;
+
+var Draw = new Schema ({
+    date : Date,
+    cashValue : Number,
+    totalRecieved : Number,
+    cashKept : Number,
+    stocks : Number,
+    bonds : Number,
+    bondsDailyRecieved : Number,
+    bondsMonthlyRecieved : Number
+});
+
+module.exports = mongoose.model('draws', Draw);
